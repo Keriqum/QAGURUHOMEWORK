711f11acb41b900dc4e7729d919c48d6973228c4
Keriqum <dmitriy2100@gmail.com>
2024-06-11 12:18:40 +0300

    Сделал косметические изменения проекта

--- src/test/java/SearchWikiSelenideTest.java
+++ src/test/java/SearchWikiSelenideTest.java
@@ -4,9 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.*;
-import static org.openqa.selenium.By.linkText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class SearchWikiSelenideTest {
 
@@ -14,13 +13,13 @@ public class SearchWikiSelenideTest {
     public static void beforeAll() {
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.baseUrl = "https://github.com";
         Configuration.pageLoadStrategy = "eager";
     }
 
     @Test
     void githubWikiTest() {
-        open("https://github.com/codeborne/selenide");
+        open("/codeborne/selenide");
         $("#wiki-tab").click();
         $(".markdown-body").find(byText("Soft assertions")).shouldHave(text("Soft assertions")).click();
         $("#wiki-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
